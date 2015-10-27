360ba12c2e02442ecec55de326c9ee19ce77c918
Ian Campelo <iancampelo@gmail.com>
2015-10-27 12:06:24 -0200

    UnPurchased added

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,11 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'firebase'])
+angular.module('todo', ['ionic', 'firebase'])
 
 .factory('Items',['$firebaseArray', function($firebaseArray){
     var itemsRef = new Firebase('https://todolistteste.firebaseio.com/items');
     return $firebaseArray(itemsRef);
   }])
-
 .controller('ListCtrl', function($scope, $ionicListDelegate, $ionicPopup, Items){
     $scope.items = Items;
     $scope.addItem = function(){
@@ -21,7 +17,8 @@ angular.module('starter', ['ionic', 'firebase'])
       }).then(function(name){
         if(name) {
           $scope.items.$add({
-            'name': name
+            'name': name,
+            'status': 'clear'
           });
         }
       });
@@ -30,6 +27,12 @@ angular.module('starter', ['ionic', 'firebase'])
       var itemRef = new Firebase('https://todolistteste.firebaseio.com/items/'+
       item.$id);
       itemRef.child('status').set('purchased');
+      $ionicListDelegate.closeOptionButtons();
+    };
+    $scope.unPurchaseItem = function(item){
+      var itemRef = new Firebase('https://todolistteste.firebaseio.com/items/'+
+        item.$id);
+      itemRef.child('status').set('clear');
       $ionicListDelegate.closeOptionButtons();
     };
   });
