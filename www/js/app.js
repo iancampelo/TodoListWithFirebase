2fa3c67fcffdb84111abe245f3f4153ba1688948
Ian Campelo <iancampelo@gmail.com>
2015-10-27 11:46:05 -0200

    Popup customized

--- www/js/app.js
+++ www/js/app.js
@@ -10,15 +10,21 @@ angular.module('starter', ['ionic', 'firebase'])
     return $firebaseArray(itemsRef);
   }])
 
-.controller('ListCtrl', function($scope, $ionicListDelegate, Items){
+.controller('ListCtrl', function($scope, $ionicListDelegate, $ionicPopup, Items){
     $scope.items = Items;
     $scope.addItem = function(){
-      var name = prompt("Todo name");
-      if(name){
-        $scope.items.$add({
-          'name': name
-        });
-      }
+      $ionicPopup.prompt({
+        title: 'Todo name',
+        template: 'Enter the TODO name',
+        inputPlaceholder: 'My new TODO item!',
+        cssClass: 'ionicModal'
+      }).then(function(name){
+        if(name) {
+          $scope.items.$add({
+            'name': name
+          });
+        }
+      });
     };
     $scope.purchaseItem = function(item){
       var itemRef = new Firebase('https://todolistteste.firebaseio.com/items/'+
